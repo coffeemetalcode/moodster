5975cea1a6831523b86888aaea2040544378cd91
doulos tou Christou <jeremyhancock32@gmail.com>
2018-11-20 20:40:21 -0500

    added connection.js and configured everything for deployment to Heroku and JawsDB

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: process.env.PORT || 3306,
+    user: "rwdakged7dmbfzp8",
+    password: "vvoihbv5mon1mx36",
+    database: "moodster_db",
+    use_env_variable: "JAWS_DB_URL"
+  });
+};
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
