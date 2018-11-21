49277db2a41b9f544a7b22426db72f618b2c8a11
doulos tou Christou <jeremyhancock32@gmail.com>
2018-11-20 21:15:36 -0500

    api-routes, first try

--- models/entry.js
+++ models/entry.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
+  var User = sequelize.define("User", {
+    id: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return Example;
+  return User;
 };
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,24 +1,24 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all users
+  app.get("/api/user", function(req, res) {
+    db.User.findAll({}).then(function(dbUsers) {
+      res.json(dbUsers);
+    });
+  });
+  //Get one user by id
+  app.get("/api/user/:id", function(req, res) {
+    db.User.findAll({ where: { id: req.params.id } }).then(function(dbUsers) {
+      res.json(dbUsers);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/user", function(req, res) {
+    db.User.create(req.body).then(function(dbUsers) {
+      res.json(dbUsers);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
 };
