0b759a524e49daa61106338aeb9d7da42f56aef9
doulos tou Christou <jeremyhancock32@gmail.com>
2018-11-23 13:47:55 -0500

    set all api routes

--- models/signup.js
+++ models/signup.js
@@ -1,6 +1,9 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    id: DataTypes.INTEGER,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
     user_name: DataTypes.TEXT,
     person_name: DataTypes.TEXT,
     phone_number: DataTypes.INTEGER
