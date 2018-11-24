a2b0c3992e89ba03eead22cfabe65a4f7338ec3f
doulos tou Christou <jeremyhancock32@gmail.com>
2018-11-24 11:05:44 -0500

    signin.html and signin.js are hooked up and new users are going to the local database

---
+++ public/js/signin.js
@@ -0,0 +1,34 @@
+// Code here handles what happens when a new user submits their information.
+// Effectively it takes the form inputs then sends it to the server to save in the DB.
+
+// when user clicks add-btn
+$("#add-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // make a newUser obj
+    var newUser = {
+      // user name from user name input
+      user_name: $("#username").val().trim(),
+      // name from name input
+      person_name: $("#name").val().trim(),
+      // phone number from mobile input
+      phone_number: $("#mobile").val().trim(),
+    };
+  
+    // send an AJAX POST-request with jQuery
+    $.post("/api/user", newUser)
+      // on success, run this callback
+      .then(function(data) {
+        // log the data we found
+        console.log(data);
+        // tell the user we're adding a user with an alert window
+        alert("Adding user...");
+      });
+  
+    // empty each input box by replacing the value with an empty string
+    $("#username").val("");
+    $("#name").val("");
+    $("#mobile").val("");
+  
+  });
+  
