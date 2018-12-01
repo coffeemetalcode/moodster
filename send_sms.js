9e7938325b1ba67e9f3700020456ceb42da4c25b
doulos tou Christou <jeremyhancock32@gmail.com>
2018-11-30 19:29:57 -0500

    everything appears to be working locally with scheduled sms from variables to all users

--- send_sms.js
+++ send_sms.js
@@ -5,47 +5,46 @@ var keys = require('./keys.js');
 const accountSid = keys.twilio.id;
 const authToken = keys.twilio.token;
 const client = require('twilio')(accountSid, authToken);
-
-var fourTimesADay = '0 8-20/4 * * *';
-var threeTimesADay = '0 8-20/6 * * *';
-var twoTimesADay = '0 8-20/12 * * *';
-var phoneNumber = 8049383949;
-var userId = 1;
-
+var request = require("request")
 var CronJob = require('cron').CronJob;
-console.log('Before job instantiation');
-const job = new CronJob(fourTimesADay, function () {
+
+var url = "https://moodster.herokuapp.com/api/user"
+var phoneNumberArray = [];
+var userIdArray = [];
+var userNameArray = [];
+var clientMessages = [];
+
+request({
+  url: url,
+  json: true
+}, function (error, response, body) {
+
+  if (!error && response.statusCode === 200) {
+    // console.log(body) // Print the json response
+    var userData = body;
+  }
+for(i=0;i<userData.length-1;i++){
+  userNameArray.push(userData[i].user_name);
+  phoneNumberArray.push(userData[i].phone_number);
+  userIdArray.push(userData[i].id);
+};
+});
+const job = new CronJob('* * * * *', function () {
+  console.log("userNameArray: " + userNameArray)
+for(i=0;i<userIdArray.length;i++){
   client.messages
     .create({
-      body: 'How are you feeling? Click below to tell Moodster!\nhttp://www.moodster.herokuapp.com/' + userId + '/entry',
+      body: 'How are you feeling ' + userNameArray[i] + '? Click below to tell Moodster!\nhttp://www.moodster.herokuapp.com/' + userIdArray[i] + '/entry',
       from: '+18046550859',
-      to: '+1' + phoneNumber
+      to: '+1' + phoneNumberArray[i]
     })
     .then(message => console.log(message.sid))
     .done();
   const d = new Date();
   console.log('onTick:', d);
+  console.log(phoneNumberArray);
+  }
 });
-
-// const http = require('http');
-// const express = require('express');
-// const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
-// const app = express();
-
-// app.post('/sms', (req, res) => {
-//   const twiml = new MessagingResponse();
-
-//   twiml.message('The Robots are coming! Head for the hills!');
-
-//   res.writeHead(200, {'Content-Type': 'text/xml'});
-//   res.end(twiml.toString());
-// });
-
-// http.createServer(app).listen(1337, () => {
-//   console.log('Express server listening on port 1337');
-// });
-
 module.exports = {
   job: job
 };
