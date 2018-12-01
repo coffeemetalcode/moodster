6948842971557d42e6d898825aaa08d5410b6d67
doulos tou Christou <jeremyhancock32@gmail.com>
2018-12-01 09:03:43 -0500

    fixed links so they go to the root url

--- send_sms.js
+++ send_sms.js
@@ -1,51 +1,51 @@
-require('dotenv').config();
-var keys = require('./keys.js');
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Your Account Sid and Auth Token from twilio.com/console
-const accountSid = keys.twilio.id;
-const authToken = keys.twilio.token;
-const client = require('twilio')(accountSid, authToken);
+// require('dotenv').config();
+// var keys = require('./keys.js');
+// // Download the helper library from https://www.twilio.com/docs/node/install
+// // Your Account Sid and Auth Token from twilio.com/console
+// const accountSid = keys.twilio.id;
+// const authToken = keys.twilio.token;
+// const client = require('twilio')(accountSid, authToken);
 
-var fourTimesADay = '0 8-20/4 * * *';
-var threeTimesADay = '0 8-20/6 * * *';
-var twoTimesADay = '0 8-20/12 * * *';
-var phoneNumber = 8049383949;
-var userId = 1;
+// var fourTimesADay = '0 8-20/4 * * *';
+// var threeTimesADay = '0 8-20/6 * * *';
+// var twoTimesADay = '0 8-20/12 * * *';
+// var phoneNumber = 8049383949;
+// var userId = 1;
 
-var CronJob = require('cron').CronJob;
-console.log('Before job instantiation');
-const job = new CronJob(fourTimesADay, function () {
-  client.messages
-    .create({
-      body: 'How are you feeling? Click below to tell Moodster!\nhttp://www.moodster.herokuapp.com/' + userId + '/entry',
-      from: '+18046550859',
-      to: '+1' + phoneNumber
-    })
-    .then(message => console.log(message.sid))
-    .done();
-  const d = new Date();
-  console.log('onTick:', d);
-});
-
-// const http = require('http');
-// const express = require('express');
-// const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
-// const app = express();
-
-// app.post('/sms', (req, res) => {
-//   const twiml = new MessagingResponse();
-
-//   twiml.message('The Robots are coming! Head for the hills!');
-
-//   res.writeHead(200, {'Content-Type': 'text/xml'});
-//   res.end(twiml.toString());
+// var CronJob = require('cron').CronJob;
+// console.log('Before job instantiation');
+// const job = new CronJob(fourTimesADay, function () {
+//   client.messages
+//     .create({
+//       body: 'How are you feeling? Click below to tell Moodster!\nhttp://www.moodster.herokuapp.com/' + userId + '/entry',
+//       from: '+18046550859',
+//       to: '+1' + phoneNumber
+//     })
+//     .then(message => console.log(message.sid))
+//     .done();
+//   const d = new Date();
+//   console.log('onTick:', d);
 // });
 
-// http.createServer(app).listen(1337, () => {
-//   console.log('Express server listening on port 1337');
-// });
+// // const http = require('http');
+// // const express = require('express');
+// // const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-module.exports = {
-  job: job
-};
+// // const app = express();
+
+// // app.post('/sms', (req, res) => {
+// //   const twiml = new MessagingResponse();
+
+// //   twiml.message('The Robots are coming! Head for the hills!');
+
+// //   res.writeHead(200, {'Content-Type': 'text/xml'});
+// //   res.end(twiml.toString());
+// // });
+
+// // http.createServer(app).listen(1337, () => {
+// //   console.log('Express server listening on port 1337');
+// // });
+
+// module.exports = {
+//   job: job
+// };
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var PORT = process.env.PORT || 8089;
 
 var db = require("./models");
-var textMessage = require("./send_sms");
+// var textMessage = require("./send_sms");
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,7 +23,7 @@ db.sequelize.sync({}).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
     console.log('After job instantiation');
-    textMessage.job.start();
+    // textMessage.job.start();
 
   });
 });
